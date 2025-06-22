97c44bfa52cf5d8676b5586743a288aba32b5b7a
Shashank Gupta <shashankg2705@gmail.com>
2025-06-22 15:03:33 +0530

    Landing Page setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/components/LandingPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import LandingNav from './partials/LandingNav'
+import Hero from './partials/Hero'
+import Working from './partials/Working'
+import Categories from './partials/Categories'
+import Reviews from './partials/Reviews'
+import Join from './partials/Join'
+import Footer from './partials/Footer'
+
+const LandingPage = () => {
+  return (
+    <div>
+        Hello
+      <LandingNav />
+      <Hero />
+      <Working />
+      <Categories />
+      <Reviews />
+      <Join />
+      <Footer />
+    </div>
+  )
+}
+
+export default LandingPage
