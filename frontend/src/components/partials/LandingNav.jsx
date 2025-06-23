9d1eda4450c6a93c8ec62accc28e8566000c73d8
Shashank Gupta <shashankg2705@gmail.com>
2025-06-23 16:51:45 +0530

    Landing Navbar

--- frontend/src/components/partials/LandingNav.jsx
+++ frontend/src/components/partials/LandingNav.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Link } from "react-router";
 
 const LandingNav = () => {
   return (
-    <div>
-      
+    <div className='grid grid-cols-2 mx-10 my-2 items-center'>
+      <div className='flex justify-evenly items-center'>
+        <p className="font-bold text-2xl text-indigo-500">SkillSwap</p>
+        <a className="font-semibold text-gray-500 hover:text-black" href="#home">Home</a>
+        <a className="font-semibold text-gray-500 hover:text-black" href="#working">How it works</a>
+        <a className="font-semibold text-gray-500 hover:text-black" href="#footer">About</a>
+      </div>
+      <div className="flex gap-5 pr-10 justify-end items-center">
+        <Link to='/signin' className="font-semibold text-gray-500 hover:text-black">Sign In</Link>
+        <Link to='/signup' className="text-white bg-indigo-500 hover:bg-indigo-600 px-4 py-2 rounded-lg">Get Started</Link>
+      </div>
     </div>
   )
 }
